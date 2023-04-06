072fa228526b714d87e07af28da64ef3def84736
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-04-06 21:10:44 +0700

    feat: sec-09

--- src/hooks/useGetServiceAPI.js
+++ src/hooks/useGetServiceAPI.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEY } from "../constants/query-key";
 import { getServiceAPI } from "../services/getServiceApi";
 
-export const useGetAboutUsAPI = () =>
+export const useGetServiceAPI = () =>
   useQuery({
     queryKey: [QUERY_KEY.SERVICE],
     queryFn: async () => {
