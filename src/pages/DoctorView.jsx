4604cd8d7b2f78ac4097e9397a0ad51b81b44ffe
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-04-07 12:05:39 +0700

    feat: sec10

--- src/pages/DoctorView.jsx
+++ src/pages/DoctorView.jsx
@@ -1,10 +1,11 @@
 import DoctorInfor from "../components/DoctorInfor";
-
+import FooterTotal from "../components/FooterTotal";
 const DoctorView = () => {
   return (
     <div>
       Doctor
       <DoctorInfor />
+      <FooterTotal />
     </div>
   );
 };
